c0709271ebf66799b97b287a9c07cb016080f1c3
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2025-07-21 23:42:02 -0300

    segundo

--- src/main/java/br/com/rafaeljavaudemy/primeiro_exemplo_udemy/PrimeiroExemploUdemyApplication.java
+++ src/main/java/br/com/rafaeljavaudemy/primeiro_exemplo_udemy/PrimeiroExemploUdemyApplication.java
@@ -1,8 +1,12 @@
 package br.com.rafaeljavaudemy.primeiro_exemplo_udemy;
 
+import br.com.rafaeljavaudemy.primeiro_exemplo_udemy.model.Produto;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class PrimeiroExemploUdemyApplication implements CommandLineRunner{
@@ -17,5 +21,8 @@ public class PrimeiroExemploUdemyApplication implements CommandLineRunner{
 		System.out.println("\n\nTEST");
 		
 	}
+
+
+
 
 }
